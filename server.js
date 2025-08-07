ed60da67fc1ba2784e23c9638989e158d6182961
KARTIKEY KUMAR <kartikeyk91@gmail.com>
2025-08-07 11:52:58 +0530

    final

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser'); // ✅ Import added
 require('dotenv').config();
+const authRoutes = require("./routes/auth")
+const postRoutes = require("./routes/posts")
+const userRoutes = require("./routes/users")
 
 const app = express();
 
@@ -22,9 +25,9 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error(err));
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/posts', require('./routes/posts'));
-app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
